75affd86a5a90265524a9579058bb23198b55345
Mister-E <morell.koerner.gera@gmail.com>
2026-07-24 12:56:29 +0200

    Add crafthead.net to image domains

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.postimg.cc",
       },
+      {
+        protocol: "https",
+        hostname: "crafthead.net",
+      },
     ],
   },
   async headers() {
